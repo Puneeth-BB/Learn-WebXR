79570b9b3019227240f11ec8040bf0a974d57946
Puneeth-BB <puneeth.kumar@balfourbeatty.com>
2021-06-15 14:10:51 +0530

    Update app.js

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -12,12 +12,26 @@ class App{
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color(0xaaaaaa);
 
-		this.renderer = new THREE.WebGLRenderer({sntialias:true});
+		const ambient = new THREE.HemisphereLight(0xffffff,0xbbbbff,0.3);
+		this.scene.add(ambient);
+
+		const light = new THREE.DirectionalLight();
+		light.position.set(0.2,1,1);
+		this.scene.add(light);
+
+		this.renderer = new THREE.WebGLRenderer({antialias:true});
 		this.renderer.setPixelRatio(window.devicePixelRatio);
 		this.renderer.setSize(window.innerWidth,window.innerHeight)
 		container.appendChild(this.renderer.domElement);
 
 		this.renderer.setAnimationLoop(this.render.bind(this));
+
+		const geometry = new THREE.BoxBufferGeometry();
+		const material = new THREE.MeshStandardMaterial({color: 0xff0000});
+
+		this.mesh = new THREE.Mesh(geometry, material);
+
+		this.scene.add(this.mesh);
 
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
@@ -26,7 +40,8 @@ class App{
         
     }
     
-	render( ) {  
+	render( ) {
+		this.mesh.rotateY(0.01);  
         this.renderer.render(this.scene, this.camera);
     }
 }
